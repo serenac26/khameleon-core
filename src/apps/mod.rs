5ccd7239fd61860e56f42d0984ca7eb68b3748aa
Serena Cheng <serenacheng99@gmail.com>
2020-12-05 14:05:11 -0600

    add game app

--- src/apps/mod.rs
+++ src/apps/mod.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, RwLock};
 
 // Available Apps
 pub mod testapp;
-
+pub mod game;
 use crate::ds;
 use crate::scheduler;
 
@@ -12,7 +12,8 @@ use crate::scheduler;
 ///          new function below, include match for the new type
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
 pub enum AppType {
-    TestApp
+    TestApp,
+    Game
 }
 
 /// apps::new: function used by the manager to create app instance
@@ -21,6 +22,7 @@ pub enum AppType {
 pub fn new(appstate: &ds::AppState, config: serde_json::Value, _state_change_flag: Arc<RwLock<bool>>) -> Box<dyn AppTrait> {
     match appstate.appname {
         AppType::TestApp => Box::new(testapp::new( appstate, config )) as Box<dyn AppTrait>,
+        AppType::Game => Box::new(game::new( appstate, config )) as Box<dyn AppTrait>,
     }
 }
 
